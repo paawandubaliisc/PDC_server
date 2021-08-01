638da0946b81b95820f45c3da2977b4fee595455
paawandubaliisc <dubalpaawan@gmail.com>
2021-08-01 09:52:07 +0530

    started

--- util.py
+++ util.py
@@ -1,1 +1,10 @@
+import socket
+import time
 
+
+class pdc:
+    SERVER = socket.gethostbyname(socket.gethostname())
+    PORT = 2345
+    SERVER_ADDR = (SERVER, PORT)
+    BUFFER = 1024
+    server = socket.socket(socket.AF_INET, socket.SOCK.DGRAM)
