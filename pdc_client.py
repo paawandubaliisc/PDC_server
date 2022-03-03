c85b70e7ded528c27fb3df22dd02b5fc470dcd6d
paawandubaliisc <dubalpaawan@gmail.com>
2022-03-03 19:10:37 +0530

    force push

--- pdc_client.py
+++ pdc_client.py
@@ -124,7 +124,7 @@ def transmit_ON(file_name, sheet_name):
     print("end")
 
 
-curr_time = 1646314514186
+curr_time = 1646314993679
 transmit_ON(file_name = "fault_on_next_line", sheet_name = dict[local_ip])
 
 
