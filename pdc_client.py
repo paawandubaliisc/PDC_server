8be2a728dd128031f15b14d8658cc9762610db6b
paawandubaliisc <dubalpaawan@gmail.com>
2022-03-07 15:13:53 +0530

    test

--- pdc_client.py
+++ pdc_client.py
@@ -133,7 +133,7 @@ def transmit_ON(file_name, sheet_name):
     print("end")
 
 
-curr_time = 1646646180301
+curr_time = 1646646408659
 transmit_ON(file_name = "fault_on_previous_line", sheet_name = dict[local_ip])
 
 
